75a00253729a63aaf2a2d02a90c79828f21aa01e
Georgi Antonov <georgy.keranov@gmail.com>
2018-01-30 02:24:44 +0200

    Added Chart.js, html and css to the admin pages.

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -13,6 +13,46 @@ class AdminController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('admin/admin.html.twig');
+        return $this->redirectToRoute("admin_dashboard");
+    }
+
+    /**
+     * @Route("/admin/dashboard", name="admin_dashboard")
+     */
+    public function dashboardAction()
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    /**
+     * @Route("/admin/messages", name="admin_messages")
+     */
+    public function messagesAction()
+    {
+        return $this->render('admin/messages.html.twig');
+    }
+
+    /**
+     * @Route("/admin/projects", name="admin_projects")
+     */
+    public function projectsAction()
+    {
+        return $this->render('admin/projects.html.twig');
+    }
+
+    /**
+     * @Route("/admin/users", name="admin_users")
+     */
+    public function usersAction()
+    {
+        return $this->render('admin/users.html.twig');
+    }
+
+    /**
+     * @Route("/admin/comments", name="admin_comments")
+     */
+    public function commentsAction()
+    {
+        return $this->render('admin/comments.html.twig');
     }
 }
